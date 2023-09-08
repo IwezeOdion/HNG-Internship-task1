9e223e64a97d39d76b73125312d4da86435675e3
IwezeOdion <iwezeodion@gmail.com>
2023-09-08 16:48:09 +0100

    HNG-Internship-task1

--- script.js
+++ script.js
@@ -13,19 +13,3 @@ function updateDateTime() {
 
   setInterval(updateDateTime, 1000);
 
-// function updateClock() {
-//   const currentTime = new Date();
-//   const options = { timeZone: 'Africa/Lagos', hour12: false };
-//   const watTime = currentTime.toLocaleTimeString('en-US', options);
-//   const watDate = currentTime.toLocaleDateString('en-US', { timeZone: 'Africa/Lagos' });
-
-//   const clockElement = document.getElementById('clock');
-//   clockElement.innerHTML = `Current Time in WAT: ${watTime} on ${watDate}`;
-//   clockElement.setAttribute('data-testid', 'currentUTCTime'); // Adding data-testid dynamically
-// }
-
-// // Update the clock every second
-// setInterval(updateClock, 1000);
-
-// // Initial call to set the clock
-// updateClock();
